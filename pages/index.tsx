c15faaadc11b29b4529f5d8f85271ce78a7554fe
Maja <maja.dabcevic@gmail.com>
2022-04-04 20:04:52 +0200

    add baseUrl

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import styles from "../styles/Home.module.scss";
+import styles from "@/styles/Home.module.scss";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 const Home: NextPage = () => {
