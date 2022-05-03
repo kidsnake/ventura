385b4f38d550f7745fed77fd780a032e0f29e26c
Petra <pelivan.petra@gmail.com>
2022-05-03 17:29:07 +0200

    add Navbar and Container

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { lightGreen, lime } from '@mui/material/colors';
+
+import Navbar from '@/components/Navbar/Navbar';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: lime[200],
+    },
+    secondary: {
+      main: lightGreen[300],
+    },
+  },
+});
 
 import '../styles/globals.scss';
 
@@ -10,7 +25,10 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
       <Head>
         <title>Ventura</title>
       </Head>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Navbar />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   );
 }
