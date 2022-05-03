6173ced5cd7ec42db9d8f9c8b09212b624ca2664
Petra <pelivan.petra@gmail.com>
2022-05-03 16:56:10 +0200

    add MaterialUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Ventura</title>
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import styles from '@/styles/Home.module.scss';
 import { useSession, signIn, signOut } from 'next-auth/react';
+import Button from '@mui/material/Button';
+
 
 const Home: NextPage = () => {
   const [counter, setCounter] = useState(0);
@@ -46,7 +47,9 @@ const Home: NextPage = () => {
           {counter}
         </h1>
 
-        <button onClick={handleSubmit}>Submit count</button>
+        <Button variant="contained"  onClick={handleSubmit} disableElevation>
+          Submit count
+        </Button>
 
         {session ? (
           <>
@@ -56,49 +59,7 @@ const Home: NextPage = () => {
         ) : (
           <button onClick={() => signIn()}>sign in</button>
         )}
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
